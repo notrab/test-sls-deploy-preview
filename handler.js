88199b683752a1a9efcdc8a5d53d2badc3979e3d
Jamie Barton <jamie@notrab.dev>
2020-01-01 10:32:09 +0000

    update

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "This is a PR deploy preview",
+        message: "This is a PR deploy preview!",
         input: event
       },
       null,
