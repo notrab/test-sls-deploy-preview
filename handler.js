e34d7ab628a5b92b5c6eba173dc7d352e0e93101
Jamie Barton <jamie@notrab.dev>
2020-01-01 10:00:44 +0000

    deploy to prod

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello deploy preview",
+        message: "Hello deploy preview!",
         input: event
       },
       null,
